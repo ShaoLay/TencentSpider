b5684243ae27300f48c32ca2c696caf94267846b
ShaoLeei <javs_shao@163.com>
2019-08-23 21:43:09 +0800

    爬取单页数据_管道文件编写_存储单页数据

--- Tencent/Tencent/pipelines.py
+++ Tencent/Tencent/pipelines.py
@@ -4,13 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
 class TencentPipeline(object):
     def open_spider(self, spider):
         self.file = open('tencentList.json', 'w')
     def process_item(self, item, spider):
-        return item
+
+        dict_data = dict(item)
+        str_data = json.dumps(dict_data) + '\n'
+        self.file.write(str_data)
 
     def close_spider(self, spider):
         self.file.close()
