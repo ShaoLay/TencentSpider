52f957c3f86a926cd50b47bb8420cb8270cc47f9
ShaoLeei <javs_shao@163.com>
2019-08-26 10:55:49 +0800

    爬取全部招聘信息

--- Tencent/Tencent/spiders/tencent.py
+++ Tencent/Tencent/spiders/tencent.py
@@ -8,20 +8,25 @@ from Tencent.items import TencentItem
 
 class TencentSpider(scrapy.Spider):
     name = 'tencent'
-    allowed_domains = ['https://careers.tencent.com/search.html']
+    allowed_domains = ['tencent.com']
     page = 1
     end_url = '&pageSize=10'
     base_url = 'https://careers.tencent.com/tencentcareer/api/post/Query?pageIndex='
     start_urls = [base_url + str(page) + end_url]
-    print('请求地址:', start_urls)
 
     def parse(self, response):
+        print('$' * 100)
         print('结果是:', response.url)
+
+        if self.page > 499:
+            return
+
         sites = json.loads(response.body_as_unicode())
         sites = sites['Data']
         sites = sites['Posts']
         for site in sites:
             item = TencentItem()
+
             item['work_name'] = site['RecruitPostName']
             item['work_type'] = site['CategoryName']
             item['work_country'] = site['CountryName']
@@ -30,3 +35,7 @@ class TencentSpider(scrapy.Spider):
             item['work_link'] = site['PostURL']
 
             yield item
+
+        self.page += 1
+        url = self.base_url + str(self.page) + self.end_url
+        yield scrapy.Request(url, callback=self.parse)
